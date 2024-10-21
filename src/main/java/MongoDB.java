f2904cd0abb99b40a2e3f237467383b0d0cbe824
hto2s <huy.to@smail.inf.h-brs.de>
2024-10-21 22:55:33 +0200

    MongoDB SetUp

--- src/main/java/MongoDB.java
+++ src/main/java/MongoDB.java
@@ -7,18 +7,17 @@ import com.mongodb.client.MongoClients;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
 
-public class QuickStart {
+public class MongoDB {
     public static void main( String[] args ) {
 
-        // Replace the placeholder with your MongoDB deployment's connection string
         String uri = "mongodb+srv://huy:jxUiHajjPiaXGRQS@cluster0.oyzan.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
 
 
         try (MongoClient mongoClient = MongoClients.create(uri)) {
-            MongoDatabase database = mongoClient.getDatabase("sample_mflix");
-            MongoCollection<Document> collection = database.getCollection("movies");
+            MongoDatabase database = mongoClient.getDatabase("PerformanceManagement");
+            MongoCollection<Document> collection = database.getCollection("Salesmen");
 
-            Document doc = collection.find(eq("title", "Back to the Future")).first();
+            Document doc = collection.find(eq("sid", 1)).first();
             if (doc != null) {
                 System.out.println(doc.toJson());
             } else {
